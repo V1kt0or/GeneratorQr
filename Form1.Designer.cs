96d94dcf8325599a2b8f7388d95f9b28f77d7284
V1kt0or <103954522+V1kt0or@users.noreply.github.com>
2022-11-25 23:52:38 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace GeneradorQr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_Subir = new System.Windows.Forms.Button();
+            this.openFileSubirArchivo = new System.Windows.Forms.OpenFileDialog();
+            this.btn_QRconv = new System.Windows.Forms.Button();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Descripcion});
+            this.dataGridView1.Location = new System.Drawing.Point(23, 74);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(498, 348);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ruta";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(69, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(304, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btn_Subir
+            // 
+            this.btn_Subir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Subir.Location = new System.Drawing.Point(391, 20);
+            this.btn_Subir.Name = "btn_Subir";
+            this.btn_Subir.Size = new System.Drawing.Size(130, 28);
+            this.btn_Subir.TabIndex = 3;
+            this.btn_Subir.Text = "Subir Archivo";
+            this.btn_Subir.UseVisualStyleBackColor = true;
+            this.btn_Subir.Click += new System.EventHandler(this.btn_Subir_Click);
+            // 
+            // openFileSubirArchivo
+            // 
+            this.openFileSubirArchivo.FileName = "openFileDialog1";
+            // 
+            // btn_QRconv
+            // 
+            this.btn_QRconv.Enabled = false;
+            this.btn_QRconv.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_QRconv.Location = new System.Drawing.Point(565, 181);
+            this.btn_QRconv.Name = "btn_QRconv";
+            this.btn_QRconv.Size = new System.Drawing.Size(148, 122);
+            this.btn_QRconv.TabIndex = 4;
+            this.btn_QRconv.Text = "Convertir a QR";
+            this.btn_QRconv.UseVisualStyleBackColor = true;
+            this.btn_QRconv.Click += new System.EventHandler(this.btn_QRconv_Click);
+            // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Codigo";
+            this.Codigo.Name = "Codigo";
+            this.Codigo.ReadOnly = true;
+            this.Codigo.Width = 150;
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.FillWeight = 200F;
+            this.Descripcion.HeaderText = "Descripción";
+            this.Descripcion.Name = "Descripcion";
+            this.Descripcion.ReadOnly = true;
+            this.Descripcion.Width = 300;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 450);
+            this.Controls.Add(this.btn_QRconv);
+            this.Controls.Add(this.btn_Subir);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_Subir;
+        private System.Windows.Forms.OpenFileDialog openFileSubirArchivo;
+        private System.Windows.Forms.Button btn_QRconv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripcion;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using iText.Barcodes;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+using iText.IO.Image;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using iText.Kernel.Geom;
+
+using Image = iText.Layout.Element.Image;
+using QRCoder;
+using System.Runtime.Remoting.Contexts;
+using HorizontalAlignment = iText.Layout.Properties.HorizontalAlignment;
+using Microsoft.Win32;
+using Path = System.IO.Path;
+using SaveFileDialog = System.Windows.Forms.SaveFileDialog;
+using iText.StyledXmlParser.Jsoup.Nodes;
+using Document = iText.Layout.Document;
+
+namespace GeneradorQr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Subir_Click(object sender, EventArgs e)
+        {
+            openFileSubirArchivo.ShowDialog();
+            String Texto = openFileSubirArchivo.FileName;
+
+            if (File.Exists(Texto) && Path.GetExtension(openFileSubirArchivo.FileName) == ".txt")
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Refresh();
+                textBox1.Text = Texto;
+                string[] Lineas = File.ReadAllLines(Texto);
+
+                
+                foreach (string linea in Lineas)
+                {
+                    string[] division = linea.Split(new string[] { " " }, 2, StringSplitOptions.None);
+        
+                    dataGridView1.Rows.Add(division[0], division[1]);
+                  
+
+                }
+
+                btn_QRconv.Enabled = true;
+                
+            }
+            else
+            {
+                MessageBox.Show("Error. Use solo archivos .txt");
+            }
+        }
+                private void btn_QRconv_Click(object sender, EventArgs e)
+                {
+                    Table table = new Table(UnitValue.CreatePercentArray(5)).UseAllAvailableWidth();
+                    var savefiledialoge = new SaveFileDialog();
+                    savefiledialoge.FileName = "Códigos " + DateTime.Now.ToString("yyyy-MM-dd");
+                    savefiledialoge.DefaultExt = ".pdf";
+                    if (savefiledialoge.ShowDialog() == DialogResult.OK)
+                    {
+                        using (FileStream stream = new FileStream(savefiledialoge.FileName, FileMode.Create))
+                        {
+
+                            PdfDocument pdfDoc = new PdfDocument(new PdfWriter(stream));
+                            Document doc = new Document(pdfDoc);
+
+                    
+                            int Cont = 0;
+                            foreach (DataGridViewRow row in dataGridView1.Rows)
+                            {
+                                if (row.Cells[0].Value != null)
+                                {
+                                    string value1 = row.Cells[0].Value.ToString();
+                                    Cell cell = new Cell().SetKeepTogether(true);
+                                    Paragraph Titulo = new Paragraph("  "+ value1).SetHorizontalAlignment(HorizontalAlignment.CENTER);
+                                    BarcodeQRCode qrcode1 = new BarcodeQRCode(row.Cells[0].Value.ToString() + "\n" + row.Cells[1].Value.ToString());
+                                    cell.Add(Titulo.SetHorizontalAlignment(HorizontalAlignment.CENTER));
+                                    cell.Add(new Image(qrcode1.CreateFormXObject(pdfDoc)).SetHeight(100));
+                                    table.AddCell(cell);
+                                    Cont++;
+                                }
+
+                            }
+                            table.SetFontSize(10);
+                            table.SetTextAlignment(TextAlignment.CENTER);
+                            doc.Add(table);
+                            doc.Close();
+
+                
+                        }
+                        
+                    }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
